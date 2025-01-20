92cf47ba1f07a3de6edd5439ea180606778053e4
andreavecino <andreavecino94@gmail.com>
2025-01-20 16:09:19 -0500

    Clase 20 de enero

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { NavController } from '@ionic/angular';
+import { Storage } from '@ionic/storage-angular';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: false,
+})
+export class LoginPage implements OnInit {
+  loginForm: FormGroup;
+  errorMessage: any;
+  formErrors = {
+    email: [
+      { type: 'required', message: 'El correo es obligatorio' },
+      { type: 'email', message: 'El correo no es valido' }
+    ]
+  }
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private navCtrl: NavController,
+    private storage: Storage
+  ) { 
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.email
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.minLength(6),
+        Validators.required
+      ]))
+    })
+  }
+
+  ngOnInit() {
+  }
+
+  loginUser(credentials: any){
+    this.authService.login(credentials).then(res => {
+      console.log(res);
+      this.errorMessage = '';
+      this.storage.set('isUserLoggedIn', true);
+      this.navCtrl.navigateForward('/home');
+    }).catch(err => {
+      console.log(err);
+      this.errorMessage = err;
+    });
+  }
+
+}
